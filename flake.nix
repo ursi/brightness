65c30b3b0003f1f706518d705aeca124efc32bbb
Mason Mackaman <masondeanm@aol.com>
2020-12-10 16:01:07 -0500

    Use simple-js

--- flake.nix
+++ flake.nix
@@ -1,21 +1,12 @@
 {
-  outputs = { self, nixpkgs }:
-    let system = "x86_64-linux"; in
-      {
-        defaultPackage.${system} = with nixpkgs.legacyPackages.${system};
-          stdenv.mkDerivation {
-            pname = "brightness";
-            version = "0.1.0";
-            buildInputs = [ nodejs ];
-            dontUnpack = true;
-            js = ./brightness.js;
+  inputs.utils.url = "/home/mason/git/flake-utils";
 
-            installPhase = ''
-              mkdir -p $out/bin
-              local ex=$out/bin/brightness
-              cp $js $ex
-              chmod +x $ex
-            '';
-          };
-      };
+  outputs = { self, nixpkgs, utils }:
+    utils.builders.simple-js {
+      inherit nixpkgs;
+      name = "brightness";
+      version = "0.1.0";
+      path = ./brightness.js;
+      systems = [ "x86_64-linux" ];
+    };
 }
