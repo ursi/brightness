f57445898ade8aa5c57b6693c694cde48a1f076e
Mason Mackaman <masondeanm@aol.com>
2020-12-16 21:15:31 -0500

    Update psnp and add it to the flake

--- flake.nix
+++ flake.nix
@@ -1,20 +1,25 @@
 {
-  outputs = { self, nixpkgs, utils }:
-    let system = "x86_64-linux"; in
-      (utils.simpleShell
-        [
-          "dhall"
-          "nodejs"
-          "purescript"
-          "spago"
-        ]
-        nixpkgs
-      )
-        // {
-        defaultPackage.${system} = import ./psnp.nix
-          rec {
-            pkgs = nixpkgs.legacyPackages.${system};
-            runtimeDeps = [ pkgs.xorg.xrandr ];
+  inputs.psnp.url = "github:ursi/psnp";
+
+  outputs = { self, nixpkgs, utils, psnp }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+      {
+        defaultPackage.${system} =
+            (import ./psnp.nix { inherit pkgs; })
+              .overrideAttrs (old: { buildInputs = [ pkgs.xorg.xrandr ] ++ old.buildInputs; });
+
+        devShell.${system} = with pkgs;
+          mkShell {
+            buildInputs = [
+              dhall
+              nodejs
+              psnp.defaultPackage.${system}
+              purescript
+              spago
+            ];
           };
       };
 }
